8f4b909d4a9605e7002f507379fc7ea870294a79
leo <leomen3@gmail.com>
2018-09-01 02:44:14 +0300

    - code cleanupo
    - fixed mqtt broker connectivity
    - fixed topics (sensor) names

--- main.py
+++ main.py
@@ -2,23 +2,16 @@
 # 1) connect to internet
 # 2) get time
 # 3) report data to RPi MQTT
-# 4) Send data to google sheets?
 ###########################################################################
 
-DEBUG = False
-
-if not DEBUG:
-    import machine
-    import utime
-    import ntptime
-    import network
-    import esp
-    from dht import DHT11
-    import ubinascii
-    from umqtt.simple import MQTTClient
-    #from umqtt.robust import MQTTClient
-else:
-    import time
+import machine
+import utime
+import ntptime
+import network
+import esp
+from dht import DHT11
+import ubinascii
+from umqtt.robust import MQTTClient
 
 
 UTC_OFFSET = 3   # hours of differenc between UTC and local (Jerusalem) time
@@ -30,10 +23,9 @@ TEMPERATURE_SAMPLE_DELAY = 5
 MOTION_LOG_DELAY = 5*60   # minimal number of seconds to wait/
                             # between subsequent motion Logging
 
-CONFIG = {
-    "broker": RPi_HOST,
-    "client_id": b"chipa_ESP8266"
-}
+
+clientID =  b"chipa_ESP8266"
+localBroker = RPi_HOST
 
 
 def toggleGPIO(p):
@@ -61,24 +53,21 @@ def getHumidityTemp():
 
 def getDateTime():
     gotTime = False
-    if DEBUG:
-        return time.localtime()
+    utime.sleep(5)  # Sleep for 5 seconds, to make sure WiFi is connected
+    if sta_if.isconnected():
+        print("Connected to internet, setting time from NTP")
+        ntptime.settime()
+    t = rtc.datetime()
+    print("RTC time is: ", t)
+    year = t[0]
+    print("year is: ",year)
+    gotTime = year > 2016
+    if gotTime:
+        print(t)
     else:
-        utime.sleep(5)  # Sleep for 5 seconds, to make sure WiFi is connected
-        if sta_if.isconnected():
-            print("Connected to internet, setting time from NTP")
-            ntptime.settime()
-        t = rtc.datetime()
-        print("RTC time is: ", t)
-        year = t[0]
-        print("year is: ",year)
-        gotTime = year > 2016
-        if gotTime:
-            print(t)
-        else:
-            print("Could not get time")
-            machine.reset()
-        return gotTime, t 
+        print("Could not get time")
+        machine.reset()
+    return gotTime, t 
 
         
 def getRPiTime():
@@ -95,8 +84,7 @@ def getRPiTime():
         s.close()
         return None
 
-    if DEBUG: 
-        print("Handshaking with RPi")
+    s.send("time".encode("utf8"))
     s.send("time".encode("utf8"))
     time.sleep(3)
     data = s.recv(1024)
@@ -124,8 +112,8 @@ def handleDHT(temper, humid):
             currentTime - lastTemperatureLogTime > TEMPERATURE_LOG_DELAY:
         print ("LOGGING temperature and humidity at: ", currentTime)
         lastTemperatureLogTime = currentTime
-        pushSample(temper,"Temperature_Chipa")
-        pushSample(humid,"Humid_Chipa")
+        pushSample(temper,"/sensor/Chipa/temperature")
+        pushSample(humid,"/sensor/Chipa/humidity")
 
 
 def handlePIR():
@@ -136,7 +124,7 @@ def handlePIR():
             (currentTime - lastMotionLogTime) > MOTION_LOG_DELAY:
         print ("LOGGING motion at: ", currentTime)
         lastMotionLogTime = currentTime
-        pushSample(lastMotionTime,"Motion_Chipa")
+        pushSample(lastMotionTime, "/sensor/Chipa/motion")
 
 
 def pushSample(sample, topic):
@@ -144,54 +132,47 @@ def pushSample(sample, topic):
     client.publish(topic, str(sample))
 
 
-def main():
-    global motionDetected, lastMotionTime
-    while True:
-        #gotTime, curr_tm = getDateTime()  # get time 
-        utime.sleep(TEMPERATURE_SAMPLE_DELAY)  # Sleep at least one second between each measurement
-
-        # sample the temperature and humidity sensor
-        temper, humid = getHumidityTemp()
-        handleDHT(temper, humid)
-
-        # if detected motion on PIR sensor
-        if motionDetected:   
-            motionDetected = False
-            handlePIR()
+def MQTT_Connect(client):
+    isMQTT_Connected = False
+    while not isMQTT_Connected:
+        try:
+            utime.sleep(1)
+            client.connect()
+            print("Connected to {}".format(localBroker))
+            isMQTT_Connected = True
+            return
+        except:
+            print("failed to connect to RPi MQTT broker. waiting 3sec and retrying")
+            utime.sleep(3)
+    
 
 #Generic Init
-if not DEBUG:
-    print ("Initializing...")
-    rtc = machine.RTC()
-    sta_if = network.WLAN(network.STA_IF)
+print ("Initializing...")
+rtc = machine.RTC()
+sta_if = network.WLAN(network.STA_IF)
 
-    motionDetected = False
-    lastMotionTime = 0
-    lastMotionLogTime = 0
+motionDetected = False
+lastMotionTime = 0
+lastMotionLogTime = 0
 
-    lastTemperatureLogTime = 0
-    dhtSensor = DHT11(machine.Pin(4))  # D2 pin on NodeMCU board. DHT signal pin
-    pirSig = machine.Pin(15, machine.Pin.IN)          # D8 pin on NodeMCU. PIR signal pin
-    pirSig.irq(handler=lambda p: pin_interrupt(), trigger=machine.Pin.IRQ_RISING)
+lastTemperatureLogTime = 0
+dhtSensor = DHT11(machine.Pin(4))  # D2 pin on NodeMCU board. DHT signal pin
+pirSig = machine.Pin(5, machine.Pin.IN)          # D1 pin on NodeMCU. PIR signal pin
+pirSig.irq(handler=lambda p: pin_interrupt(), trigger=machine.Pin.IRQ_RISING)
 
-    #MQTT configs
-    client = MQTTClient(CONFIG['client_id'], CONFIG['broker'])
-    try:
-        utime.sleep(3)
-        client.connect()
-        print("Connected to {}".format(CONFIG['broker']))
-        print ("Initializing Done")
-    except:
-        print("failed to connect to RPi. waiting 5sec and resetting")
-        utime.sleep(5)
-        machine.reset()
+#MQTT configs
+client = MQTTClient(clientID, localBroker)
+MQTT_Connect(client)
 
-if DEBUG:
-    if __name__ == "__main__":
-        main() 
-else:
-    main()
+while True:
+    #gotTime, curr_tm = getDateTime()  # get time 
+    utime.sleep(TEMPERATURE_SAMPLE_DELAY)  # Sleep at least one second between each measurement
 
-#TODO
-# Add logging
-# Add ADC reading
+    # sample the temperature and humidity sensor
+    temper, humid = getHumidityTemp()
+    handleDHT(temper, humid)
+
+    # if detected motion on PIR sensor
+    if motionDetected:   
+        motionDetected = False
+        handlePIR()
